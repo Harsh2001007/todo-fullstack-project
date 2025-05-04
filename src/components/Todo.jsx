7f80632eba6fc6f4bff54ba5f804302a99c667f5
Harsh <hs5686584@gmail.com>
2025-05-05 00:16:05 +0530

    added self sign

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import todo_img from "./../assets/todo_icon.png";
+import cust from "./../assets/cust.png";
+import port from "./../assets/port.png";
+import portTrans from "./../assets/portTrans.png";
 import TodoItems from "./TodoItems";
 
 const Todo = () => {
@@ -52,45 +55,51 @@ const Todo = () => {
   }, [todoList]);
 
   return (
-    <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
-      {/* Title  */}
-
-      <div className="flex items-center mt-7 gap-2">
-        <img src={todo_img} alt="" className="w-8" />
-        <h1 className="text-3xl font-semibold">TO-DO List</h1>
+    <div>
+      <div className="place-self-center mt-10 flex flex-row justify-center items-center">
+        <p className="text-white italic underline">Made by Harsh Sachan</p>
+        <img src={portTrans} alt="" className="w-30" />
       </div>
+      <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl mt-5">
+        {/* Title  */}
 
-      {/* Input box */}
+        <div className="flex items-center mt-7 gap-2">
+          <img src={todo_img} alt="" className="w-8" />
+          <h1 className="text-3xl font-semibold">TO-DO List</h1>
+        </div>
 
-      <div className="flex items-center my-7 bg-gray-200 rounded-full">
-        <input
-          type="text"
-          placeholder="Add your tasks"
-          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-500"
-          ref={inputRef}
-        />
-        <button
-          onClick={addTask}
-          className="border-none rounded-full bg-purple-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
-        >
-          ADD +
-        </button>
-      </div>
+        {/* Input box */}
 
-      {/* Todo list  */}
-      <div>
-        {todoList.map((item, index) => (
-          <TodoItems
-            text={item.text}
-            key={index}
-            isComplete={item.isComplete}
-            id={item.id}
-            onDelete={deleteTask}
-            toggle={toggle}
+        <div className="flex items-center my-7 bg-gray-200 rounded-full">
+          <input
+            type="text"
+            placeholder="Add your tasks"
+            className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-500"
+            ref={inputRef}
           />
-        ))}
-        {/* <TodoItems text={"learn express"} />
+          <button
+            onClick={addTask}
+            className="border-none rounded-full bg-purple-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+          >
+            ADD +
+          </button>
+        </div>
+
+        {/* Todo list  */}
+        <div>
+          {todoList.map((item, index) => (
+            <TodoItems
+              text={item.text}
+              key={index}
+              isComplete={item.isComplete}
+              id={item.id}
+              onDelete={deleteTask}
+              toggle={toggle}
+            />
+          ))}
+          {/* <TodoItems text={"learn express"} />
         <TodoItems text={"learn react"} /> */}
+        </div>
       </div>
     </div>
   );
